3b86f65561632d5f26b58e696a99766401f31324
Jenkin <jiancun.zhao@70capital.com>
2020-10-23 17:16:52 +0800

    fixes bug

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const form = querySelectorAll('[name="verify"]');
-const inputs = querySelectorAll('.inputs input');
+const form = document.querySelectorAll('[name="verify"]');
+const inputs = document.querySelectorAll('.inputs input');
 
 function handleInput(e) {
   // check for data that was inputtted and if there is a next input, focus it
   const input = e.target;
-  console.log(input);
   if(input.nextElementSibling && input.value) {
     input.nextElementSibling.focus();
   }
 }
 
+function handleDelete(e) {
+  const current = e.target;
+  // clear current and move to the previous input box
+  if(e.keyCode === 8 && current.previousElementSibling) {
+    e.preventDefault();
+    current.value = '' ;
+    current.previousElementSibling.focus();
+  }
+}
+
 function handlePaste(e) {
-  const paste = e.clipboardData.getData('text');
+  const paste = e.clipboardData.getData('text/plain');
   // loop over each input, and populate with the index of that string
   inputs.forEach((input, i) => {
-    console.log(input);
     input.value = paste[i] || '';
   });
 }
 
 inputs[0].addEventListener('paste', handlePaste);
-
-from.addEventListener('input', handleInput);
+form[0].addEventListener('keydown', handleDelete);
+form[0].addEventListener('input', handleInput);
 
 // 1. select the text when the next input is focued
 // 2. Auto submit the form if all fields are filled after a paste
